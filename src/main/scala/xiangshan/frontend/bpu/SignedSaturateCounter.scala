OpenXiangShan/XiangShan

refactor(SaturateCounter): rewrite for easier use

src/main/scala/xiangshan/frontend/bpu/SignedSaturateCounter.scala
@@ -17,46 +17,120 @@ package xiangshan.frontend.bpu
 
 import chisel3._
 
-// TODO: maybe move to utility?
 class SignedSaturateCounter(width: Int) extends Bundle {
   val value: SInt = SInt(width.W)
 
+  /* *** state methods *** */
+  // direction
   def isPositive: Bool = value >= 0.S
-
-  def isSaturatePositive: Bool = value === ((1 << (width - 1)) - 1).S
-
   def isNegative: Bool = value < 0.S
+  // saturated
+  def isSaturatePositive: Bool = value === ((1 << (width - 1)) - 1).S
+  def isSaturateNegative: Bool = value === (-(1 << (width - 1))).S
+  def isSaturate:         Bool = isSaturatePositive || isSaturateNegative
+  def shouldHold(positive: Bool): Bool =
+    isSaturatePositive && positive || isSaturateNegative && !positive
 
-  def isSaturateNegative: Bool = value === (-(1 << (width - 1))).S // scalastyle:ignore
+  // weak
+  def isWeakPositive: Bool = { // value === 1.S
+    require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+    value === 1.S
+  }
+  def isWeakNegative: Bool = { // value === -1.S
+    require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+    value === -1.S
+  }
+  def isWeak: Bool = isWeakPositive || isWeakNegative
 
-  def getUpdate(positive: Bool): SInt = Mux(
-    isSaturatePositive && positive || isSaturateNegative && !positive,
-    value,
-    Mux(positive, value + 1.S, value - 1.S)
-  )
+  /* *** private update methods *** */
+  private def getUpdatedValue(positive: Bool, en: Bool): SInt =
+    // hold if already saturated on the direction
+    Mux(!en || shouldHold(positive), value, Mux(positive, value + 1.S, value - 1.S))
 
-  def getIncrease: SInt = Mux(isSaturatePositive, value, value + 1.S)
+  private def getIncreasedValue(en: Bool): SInt =
+    Mux(!en || isSaturatePositive, value, value + 1.S)
 
-  def getDecrease: SInt = Mux(isSaturateNegative, value, value - 1.S)
+  private def getDecreasedValue(en: Bool): SInt =
+    Mux(!en || isSaturateNegative, value, value - 1.S)
 
-  def update(positive: Bool): Unit =
-    value := getUpdate(positive)
+  /* *** update method for wires *** */
+  def update(increase: Bool, en: Bool = true.B): SignedSaturateCounter =
+    SignedSaturateCounterInit(this.width, getUpdatedValue(increase, en))
 
-  def increase(): Unit =
-    value := getIncrease
+  def increase(en: Bool = true.B): SignedSaturateCounter =
+    SignedSaturateCounterInit(this.width, getIncreasedValue(en))
 
-  def decrease(): Unit =
-    value := getDecrease
+  def decrease(en: Bool = true.B): SignedSaturateCounter =
+    SignedSaturateCounterInit(this.width, getDecreasedValue(en))
 
+  /* *** update method for regs *** */
+  def updateSelf(increase: Bool, en: Bool = true.B): Unit =
+    value := getUpdatedValue(increase, en)
+
+  def increaseSelf(en: Bool = true.B): Unit =
+    value := getIncreasedValue(en)
+
+  def decreaseSelf(en: Bool = true.B): Unit =
+    value := getDecreasedValue(en)
+
+  /* *** reset method for regs *** */
   def resetZero(): Unit =
-    value := 0.S
+    value := 0.U
 
-  def resetNegative(): Unit =
-    value := -(1 << (width - 1)).S // scalastyle:ignore
+  def resetSaturatePositive(): Unit =
+    value := SignedSaturateCounter.Value.SaturatePositive(this.width).S
 
-  def resetNeutral(): Unit =
-    value := 0.S
+  def resetSaturateNegative(): Unit =
+    value := SignedSaturateCounter.Value.SaturateNegative(this.width).S
 
-  def resetPositive(): Unit =
-    value := ((1 << (width - 1)) - 1).S
+  def resetWeakPositive(): Unit =
+    value := SignedSaturateCounter.Value.WeakPositive(this.width).S
+
+  def resetWeakNegative(): Unit =
+    value := SignedSaturateCounter.Value.WeakNegative(this.width).S
+}
+
+object SignedSaturateCounter {
+  object Value {
+    def SaturatePositive(width: Int): Int = (1 << (width - 1)) - 1
+    def SaturateNegative(width: Int): Int = -(1 << (width - 1))
+    def WeakPositive(width: Int): Int = {
+      require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+      1
+    }
+    def WeakNegative(width: Int): Int = {
+      require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+      -1
+    }
+  }
+
+  def SaturatePositive(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.SaturatePositive(width))
+
+  def SaturateNegative(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.SaturateNegative(width))
+
+  def WeakPositive(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.WeakPositive(width))
+
+  def WeakNegative(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.WeakNegative(width))
+
+  // replace `0.U.asTypeOf(SignedSaturateCounter(width))` with `SignedSaturateCounter.Zero(width)`
+  def Zero(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, 0)
+}
+
+object SignedSaturateCounterInit {
+  def apply(width: Int, init: Int): SignedSaturateCounter = {
+    val counter = Wire(new SignedSaturateCounter(width))
+    counter.value := init.S
+    counter
+  }
+
+  def apply(width: Int, init: SInt): SignedSaturateCounter = {
+    val counter = Wire(new SignedSaturateCounter(width))
+    counter.value := init
+    counter
+  }
 }

src/main/scala/xiangshan/frontend/bpu/mbtb/MainBtbAlignBank.scala
@@ -245,8 +245,12 @@ class MainBtbAlignBank(
 
     val entryOverridden = t1_entryNeedWrite && t1_entryWayMask(i)
 
-    t1_counterWayMask(i)    := entryOverridden || hitMask.reduce(_ || _)
-    t1_newCounters(i).value := Mux(entryOverridden, meta.counter.getWeakPositive(), meta.counter.getUpdate(actualTaken))
+    t1_counterWayMask(i) := entryOverridden || hitMask.reduce(_ || _)
+    t1_newCounters(i) := Mux(
+      entryOverridden,
+      SaturateCounter.WeakPositive(TakenCntWidth),
+      meta.counter.update(actualTaken)
+    )
   }
 
   // write counter anytime when needed

src/main/scala/xiangshan/frontend/bpu/tage/Tage.scala
@@ -28,6 +28,7 @@ import xiangshan.frontend.bpu.BasePredictorIO
 import xiangshan.frontend.bpu.HalfAlignHelper
 import xiangshan.frontend.bpu.Prediction
 import xiangshan.frontend.bpu.SaturateCounter
+import xiangshan.frontend.bpu.SaturateCounterInit
 import xiangshan.frontend.bpu.TageTableInfo
 import xiangshan.frontend.bpu.history.phr.PhrAllFoldedHistories
 
@@ -161,8 +162,8 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     val providerTableOH  = getLongestHistTableOH(hitTableMask)
     val providerTakenCtr = Mux1H(providerTableOH, allTableTagMatchResults.map(_.entry.takenCtr))
 
-    s2_providerTakenCtrVec(i).valid      := hasProvider
-    s2_providerTakenCtrVec(i).bits.value := providerTakenCtr.value
+    s2_providerTakenCtrVec(i).valid := hasProvider
+    s2_providerTakenCtrVec(i).bits  := providerTakenCtr
 
     // find the alt, the table with the second longest history among the hit tables
     val hitTableMaskNoProvider = hitTableMask.zip(providerTableOH).map { case (a, b) => a && !b }
@@ -369,34 +370,30 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
       t2_valid && branch.valid && isCond && ((finalPred =/= actualTaken) =/= branch.bits.mispredict)
     )
 
-    val providerNewTakenCtr       = providerInfo.entry.takenCtr.getUpdate(actualTaken)
+    val providerNewTakenCtr       = providerInfo.entry.takenCtr.update(actualTaken)
     val increaseProviderUsefulCtr = hasProvider && pred === actualTaken && pred =/= Mux(hasAlt, altPred, basePred)
-    val providerNewUsefulCtr = Mux(
-      increaseProviderUsefulCtr,
-      providerInfo.usefulCtr.getIncrease,
-      providerInfo.usefulCtr.value
-    )
-    val altNewTakenCtr = altInfo.entry.takenCtr.getUpdate(actualTaken)
+    val providerNewUsefulCtr      = providerInfo.usefulCtr.increase(increaseProviderUsefulCtr)
+    val altNewTakenCtr            = altInfo.entry.takenCtr.update(actualTaken)
 
     val increaseUseAlt = hasProvider && providerIsWeak && Mux(hasAlt, altPred, basePred) === actualTaken
     val decreaseUseAlt = hasProvider && providerIsWeak && Mux(hasAlt, altPred, basePred) =/= actualTaken
 
     val updateInfo = Wire(new UpdateInfo).suggestName(s"branch_${brIdx}_updateInfo")
-    updateInfo.valid                        := isCond // Only consider update if conditional branch
-    updateInfo.providerTableOH              := providerTableOH.asUInt & Fill(NumTables, hasProvider)
-    updateInfo.providerWayOH                := providerInfo.hitWayMaskOH
-    updateInfo.providerEntry.valid          := true.B
-    updateInfo.providerEntry.tag            := providerInfo.entry.tag
-    updateInfo.providerEntry.takenCtr.value := providerNewTakenCtr
-    updateInfo.providerOldUsefulCtr         := providerInfo.usefulCtr
-    updateInfo.providerNewUsefulCtr.value   := providerNewUsefulCtr
+    updateInfo.valid                  := isCond // Only consider update if conditional branch
+    updateInfo.providerTableOH        := providerTableOH.asUInt & Fill(NumTables, hasProvider)
+    updateInfo.providerWayOH          := providerInfo.hitWayMaskOH
+    updateInfo.providerEntry.valid    := true.B
+    updateInfo.providerEntry.tag      := providerInfo.entry.tag
+    updateInfo.providerEntry.takenCtr := providerNewTakenCtr
+    updateInfo.providerOldUsefulCtr   := providerInfo.usefulCtr
+    updateInfo.providerNewUsefulCtr   := providerNewUsefulCtr
 
-    updateInfo.altTableOH              := altTableOH.asUInt & Fill(NumTables, useAlt)
-    updateInfo.altWayOH                := altInfo.hitWayMaskOH
-    updateInfo.altEntry.valid          := true.B
-    updateInfo.altEntry.tag            := altInfo.entry.tag
-    updateInfo.altEntry.takenCtr.value := altNewTakenCtr
-    updateInfo.altOldUsefulCtr         := altInfo.usefulCtr
+    updateInfo.altTableOH        := altTableOH.asUInt & Fill(NumTables, useAlt)
+    updateInfo.altWayOH          := altInfo.hitWayMaskOH
+    updateInfo.altEntry.valid    := true.B
+    updateInfo.altEntry.tag      := altInfo.entry.tag
+    updateInfo.altEntry.takenCtr := altNewTakenCtr
+    updateInfo.altOldUsefulCtr   := altInfo.usefulCtr
 
     updateInfo.useAlt    := useAlt
     updateInfo.finalPred := finalPred
@@ -423,9 +420,9 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     }.reduce(_ || _)
 
     when(t2_valid && increase) {
-      ctr.increase()
+      ctr.increaseSelf()
     }.elsewhen(t2_valid && decrease) {
-      ctr.decrease()
+      ctr.decreaseSelf()
     }
   }
 
@@ -448,7 +445,7 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
   private val t2_allTableCanAllocateWayMask =
     t2_allTableEntries.zip(t2_allTableUsefulCtrs).map { case (entriesPerTable, ctrsPerTable) =>
       entriesPerTable.zip(ctrsPerTable).map { case (entry, usefulCtr) =>
-        !entry.valid || entry.valid && entry.takenCtr.isWeak && usefulCtr.value === 0.U
+        !entry.valid || entry.valid && entry.takenCtr.isWeak && usefulCtr.isSaturateNegative
       }.asUInt
     }
   private val t2_canAllocateTableMask = t2_longerHistoryTableMask & t2_allTableCanAllocateWayMask.map(_.orR).asUInt
@@ -464,10 +461,10 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
   private val t2_allocateEntry = Wire(new TageEntry)
   t2_allocateEntry.valid := true.B
   t2_allocateEntry.tag   := Mux1H(t2_allocateTableMaskOH, t2_rawTag) ^ t2_allocateBranch.bits.cfiPosition
-  t2_allocateEntry.takenCtr.value := Mux(
+  t2_allocateEntry.takenCtr := Mux(
     t2_allocateBranch.bits.taken,
-    (1 << (TakenCtrWidth - 1)).U,      // weak taken
-    ((1 << (TakenCtrWidth - 1)) - 1).U // weak not taken
+    SaturateCounter.WeakPositive(TakenCtrWidth), // weak taken
+    SaturateCounter.WeakNegative(TakenCtrWidth)  // weak not taken
   )
 
   tables.zipWithIndex.foreach { case (table, tableIdx) =>
@@ -507,8 +504,12 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     writeEntries.zip(writeUsefulCtrs).zipWithIndex.foreach { case ((entry, usefulCtr), wayIdx) =>
       val thisWayNeedUpdate   = updateMask(wayIdx)
       val thisWayNeedAllocate = thisTableNeedAllocate && t2_allocateWayMaskOH(wayIdx)
-      entry                := Mux(thisWayNeedAllocate, t2_allocateEntry, updateEntries(wayIdx))
-      usefulCtr.value      := Mux(thisWayNeedAllocate, UsefulCtrInitValue.U, updateUsefulCtrs(wayIdx).value)
+      entry := Mux(thisWayNeedAllocate, t2_allocateEntry, updateEntries(wayIdx))
+      usefulCtr := Mux(
+        thisWayNeedAllocate,
+        SaturateCounterInit(UsefulCtrWidth, UsefulCtrInitValue),
+        updateUsefulCtrs(wayIdx)
+      )
       writeWayMask(wayIdx) := thisWayNeedUpdate || thisWayNeedAllocate
     }
 
@@ -522,7 +523,7 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
   when(t2_usefulReset) {
     usefulResetCtr.resetZero()
   }.elsewhen(t2_valid && t2_needAllocate && !t2_canAllocate) {
-    usefulResetCtr.increase()
+    usefulResetCtr.increaseSelf()
   }
 
   /* --------------------------------------------------------------------------------------------------------------

src/main/scala/xiangshan/frontend/bpu/ubtb/MicroBtb.scala
@@ -175,7 +175,7 @@ class MicroBtb(implicit p: Parameters) extends BasePredictor with HasMicroBtbPar
   private def initEntryIfNotUseful(notUseful: Bool): Unit =
     when(notUseful) {
       t1_updatedEntry.tag := t1_tag
-      t1_updatedEntry.usefulCnt.resetPositive() // usefulCnt inits at strong positive, in/decrease by policy
+      t1_updatedEntry.usefulCnt.resetSaturatePositive() // usefulCnt inits at strong positive, in/decrease by policy
       // slot1
       t1_updatedEntry.slot1.position       := t1_position
       t1_updatedEntry.slot1.attribute      := t1_attribute
@@ -185,7 +185,7 @@ class MicroBtb(implicit p: Parameters) extends BasePredictor with HasMicroBtbPar
       // TODO: 2-taken train
       t1_updatedEntry.slot2.valid := false.B
     }.otherwise {
-      t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getDecrease
+      t1_updatedEntry.usefulCnt := t1_hitEntry.usefulCnt.decrease()
     }
 
   when(t1_valid) {
@@ -204,7 +204,7 @@ class MicroBtb(implicit p: Parameters) extends BasePredictor with HasMicroBtbPar
     }.otherwise {
       // everything matches, and actually taken
       // increase usefulCnt
-      t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getIncrease
+      t1_updatedEntry.usefulCnt := t1_hitEntry.usefulCnt.increase()
     }
   }
 

R-EAjks-Compute/OpenXiangShan

src/main/scala/xiangshan/frontend/bpu/SignedSaturateCounter.scala
@@ -17,46 +17,132 @@ package xiangshan.frontend.bpu
 
 import chisel3._
 
-// TODO: maybe move to utility?
 class SignedSaturateCounter(width: Int) extends Bundle {
   val value: SInt = SInt(width.W)
 
+  // === is defined in Bundle, but =/= is not
+  def =/=(that: SignedSaturateCounter): Bool = this.value =/= that.value // scalastyle:ignore method.name
+  def <(that:   SignedSaturateCounter): Bool = this.value < that.value
+  def <=(that:  SignedSaturateCounter): Bool = this.value <= that.value
+  def >(that:   SignedSaturateCounter): Bool = this.value > that.value
+  def >=(that:  SignedSaturateCounter): Bool = this.value >= that.value
+
+  /* *** state methods *** */
+  // direction
   def isPositive: Bool = value >= 0.S
-
-  def isSaturatePositive: Bool = value === ((1 << (width - 1)) - 1).S
-
   def isNegative: Bool = value < 0.S
+  // saturated
+  def isSaturatePositive: Bool = value === ((1 << (width - 1)) - 1).S
+  def isSaturateNegative: Bool = value === (-(1 << (width - 1))).S
+  def isSaturate:         Bool = isSaturatePositive || isSaturateNegative
+  def shouldHold(positive: Bool): Bool =
+    isSaturatePositive && positive || isSaturateNegative && !positive
 
-  def isSaturateNegative: Bool = value === (-(1 << (width - 1))).S // scalastyle:ignore
+  // weak
+  def isWeakPositive: Bool = { // value === 1.S
+    require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+    value === 1.S
+  }
+  def isWeakNegative: Bool = { // value === -1.S
+    require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+    value === -1.S
+  }
+  def isWeak: Bool = isWeakPositive || isWeakNegative
+  // medium
+  def isMid: Bool = {
+    require(width >= 3, "SaturateCounter width must be at least 3 to have mid states")
+    !isSaturate && !isWeak
+  }
 
-  def getUpdate(positive: Bool): SInt = Mux(
-    isSaturatePositive && positive || isSaturateNegative && !positive,
-    value,
-    Mux(positive, value + 1.S, value - 1.S)
-  )
+  /* *** private update methods *** */
+  private def getUpdatedValue(positive: Bool, en: Bool): SInt =
+    // hold if already saturated on the direction
+    Mux(!en || shouldHold(positive), value, Mux(positive, value + 1.S, value - 1.S))
 
-  def getIncrease: SInt = Mux(isSaturatePositive, value, value + 1.S)
+  private def getIncreasedValue(en: Bool): SInt =
+    Mux(!en || isSaturatePositive, value, value + 1.S)
 
-  def getDecrease: SInt = Mux(isSaturateNegative, value, value - 1.S)
+  private def getDecreasedValue(en: Bool): SInt =
+    Mux(!en || isSaturateNegative, value, value - 1.S)
 
-  def update(positive: Bool): Unit =
-    value := getUpdate(positive)
+  /* *** update method for wires *** */
+  def getUpdate(increase: Bool, en: Bool = true.B): SignedSaturateCounter =
+    SignedSaturateCounterInit(this.width, getUpdatedValue(increase, en))
 
-  def increase(): Unit =
-    value := getIncrease
+  def getIncrease(en: Bool = true.B): SignedSaturateCounter =
+    SignedSaturateCounterInit(this.width, getIncreasedValue(en))
 
-  def decrease(): Unit =
-    value := getDecrease
+  def getDecrease(en: Bool = true.B): SignedSaturateCounter =
+    SignedSaturateCounterInit(this.width, getDecreasedValue(en))
 
+  /* *** update method for regs *** */
+  def selfUpdate(increase: Bool, en: Bool = true.B): Unit =
+    value := getUpdatedValue(increase, en)
+
+  def selfIncrease(en: Bool = true.B): Unit =
+    value := getIncreasedValue(en)
+
+  def selfDecrease(en: Bool = true.B): Unit =
+    value := getDecreasedValue(en)
+
+  /* *** reset method for regs *** */
   def resetZero(): Unit =
-    value := 0.S
+    value := 0.U
 
-  def resetNegative(): Unit =
-    value := -(1 << (width - 1)).S // scalastyle:ignore
+  def resetSaturatePositive(): Unit =
+    value := SignedSaturateCounter.Value.SaturatePositive(this.width).S
 
-  def resetNeutral(): Unit =
-    value := 0.S
+  def resetSaturateNegative(): Unit =
+    value := SignedSaturateCounter.Value.SaturateNegative(this.width).S
 
-  def resetPositive(): Unit =
-    value := ((1 << (width - 1)) - 1).S
+  def resetWeakPositive(): Unit =
+    value := SignedSaturateCounter.Value.WeakPositive(this.width).S
+
+  def resetWeakNegative(): Unit =
+    value := SignedSaturateCounter.Value.WeakNegative(this.width).S
+}
+
+object SignedSaturateCounter {
+  object Value {
+    def SaturatePositive(width: Int): Int = (1 << (width - 1)) - 1
+    def SaturateNegative(width: Int): Int = -(1 << (width - 1))
+    def WeakPositive(width: Int): Int = {
+      require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+      1
+    }
+    def WeakNegative(width: Int): Int = {
+      require(width >= 2, "SignedSaturateCounter width must be at least 2 to have weak states")
+      -1
+    }
+  }
+
+  def SaturatePositive(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.SaturatePositive(width))
+
+  def SaturateNegative(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.SaturateNegative(width))
+
+  def WeakPositive(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.WeakPositive(width))
+
+  def WeakNegative(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, Value.WeakNegative(width))
+
+  // replace `0.U.asTypeOf(SignedSaturateCounter(width))` with `SignedSaturateCounter.Zero(width)`
+  def Zero(width: Int): SignedSaturateCounter =
+    SignedSaturateCounterInit(width, 0)
+}
+
+object SignedSaturateCounterInit {
+  def apply(width: Int, init: Int): SignedSaturateCounter = {
+    val counter = Wire(new SignedSaturateCounter(width))
+    counter.value := init.S
+    counter
+  }
+
+  def apply(width: Int, init: SInt): SignedSaturateCounter = {
+    val counter = Wire(new SignedSaturateCounter(width))
+    counter.value := init
+    counter
+  }
 }

src/main/scala/xiangshan/frontend/bpu/mbtb/MainBtbAlignBank.scala
@@ -257,8 +257,12 @@ class MainBtbAlignBank(
 
     val entryOverridden = t1_entryNeedWrite && t1_entryWayMask(i)
 
-    t1_counterWayMask(i)    := entryOverridden || hitMask.reduce(_ || _)
-    t1_newCounters(i).value := Mux(entryOverridden, meta.counter.getWeakPositive(), meta.counter.getUpdate(actualTaken))
+    t1_counterWayMask(i) := entryOverridden || hitMask.reduce(_ || _)
+    t1_newCounters(i) := Mux(
+      entryOverridden,
+      SaturateCounter.WeakPositive(TakenCntWidth),
+      meta.counter.getUpdate(actualTaken)
+    )
   }
 
   // write counter anytime when needed

src/main/scala/xiangshan/frontend/bpu/tage/Tage.scala
@@ -27,6 +27,7 @@ import xiangshan.frontend.bpu.BasePredictor
 import xiangshan.frontend.bpu.BasePredictorIO
 import xiangshan.frontend.bpu.HalfAlignHelper
 import xiangshan.frontend.bpu.SaturateCounter
+import xiangshan.frontend.bpu.SaturateCounterInit
 import xiangshan.frontend.bpu.TageTableInfo
 
 /**
@@ -153,8 +154,8 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     io.prediction(i).hasAlt       := hasAlt
     io.prediction(i).altPred      := alt.takenCtr.isPositive
 
-    io.toSc.providerTakenCtrVec(i).valid      := hasProvider
-    io.toSc.providerTakenCtrVec(i).bits.value := provider.takenCtr.value
+    io.toSc.providerTakenCtrVec(i).valid := hasProvider
+    io.toSc.providerTakenCtrVec(i).bits  := provider.takenCtr
 
     io.meta.entries(i).useProvider       := useProvider
     io.meta.entries(i).providerTableIdx  := OHToUInt(providerTableOH)
@@ -389,7 +390,7 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     val altNewTakenCtr      = alt.takenCtr.getUpdate(actualTaken)
 
     val incProviderUsefulCtr = hasProvider && providerPred === actualTaken && providerPred =/= altOrBasePred
-    val providerNewUsefulCtr = Mux(incProviderUsefulCtr, provider.usefulCtr.getIncrease, provider.usefulCtr.value)
+    val providerNewUsefulCtr = provider.usefulCtr.getIncrease(incProviderUsefulCtr)
 
     // allocate when mispredict, but except when:
     // 1. already on the highest table
@@ -410,24 +411,24 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     val trainInfo = Wire(new TrainInfo).suggestName(s"t2_branch_${i}_trainInfo")
     trainInfo.valid := isCond && mbtbHit // Only consider update if conditional branch
 
-    trainInfo.hasProvider                  := hasProvider
-    trainInfo.useProvider                  := useProvider
-    trainInfo.providerTableOH              := providerTableOH.asUInt
-    trainInfo.providerWayOH                := provider.hitWayMaskOH
-    trainInfo.providerEntry.valid          := true.B
-    trainInfo.providerEntry.tag            := provider.tag
-    trainInfo.providerEntry.takenCtr.value := providerNewTakenCtr
-    trainInfo.providerOldUsefulCtr         := provider.usefulCtr
-    trainInfo.providerNewUsefulCtr.value   := providerNewUsefulCtr
+    trainInfo.hasProvider            := hasProvider
+    trainInfo.useProvider            := useProvider
+    trainInfo.providerTableOH        := providerTableOH.asUInt
+    trainInfo.providerWayOH          := provider.hitWayMaskOH
+    trainInfo.providerEntry.valid    := true.B
+    trainInfo.providerEntry.tag      := provider.tag
+    trainInfo.providerEntry.takenCtr := providerNewTakenCtr
+    trainInfo.providerOldUsefulCtr   := provider.usefulCtr
+    trainInfo.providerNewUsefulCtr   := providerNewUsefulCtr
 
-    trainInfo.hasAlt                  := hasAlt
-    trainInfo.useAlt                  := useAlt
-    trainInfo.altTableOH              := altTableOH.asUInt
-    trainInfo.altWayOH                := alt.hitWayMaskOH
-    trainInfo.altEntry.valid          := true.B
-    trainInfo.altEntry.tag            := alt.tag
-    trainInfo.altEntry.takenCtr.value := altNewTakenCtr
-    trainInfo.altOldUsefulCtr         := alt.usefulCtr
+    trainInfo.hasAlt            := hasAlt
+    trainInfo.useAlt            := useAlt
+    trainInfo.altTableOH        := altTableOH.asUInt
+    trainInfo.altWayOH          := alt.hitWayMaskOH
+    trainInfo.altEntry.valid    := true.B
+    trainInfo.altEntry.tag      := alt.tag
+    trainInfo.altEntry.takenCtr := altNewTakenCtr
+    trainInfo.altOldUsefulCtr   := alt.usefulCtr
 
     trainInfo.needAllocate       := needAllocate
     trainInfo.needUpdateProvider := needUpdateProvider
@@ -467,7 +468,7 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
 
   private val t2_allTableCanAllocateWayMask = t2_readResp.map { tableReadResp =>
     tableReadResp.entries.zip(tableReadResp.usefulCtrs).map { case (entry, usefulCtr) =>
-      !entry.valid || entry.valid && entry.takenCtr.isWeak && usefulCtr.value === 0.U
+      !entry.valid || entry.valid && entry.takenCtr.isWeak && usefulCtr.isSaturateNegative
     }.asUInt
   }
   private val t2_canAllocateTableMask = t2_longerHistoryTableMask & t2_allTableCanAllocateWayMask.map(_.orR).asUInt
@@ -487,10 +488,10 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     val entry       = Wire(new TageEntry)
     entry.valid := true.B
     entry.tag   := rawTag ^ position
-    entry.takenCtr.value := Mux(
+    entry.takenCtr := Mux(
       actualTaken,
-      (1 << (TakenCtrWidth - 1)).U,      // weak taken
-      ((1 << (TakenCtrWidth - 1)) - 1).U // weak not taken
+      SaturateCounter.WeakPositive(TakenCtrWidth), // weak taken
+      SaturateCounter.WeakNegative(TakenCtrWidth)  // weak not taken
     )
     entry
   }
@@ -551,7 +552,7 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
     when(usefulResetCtr.isSaturatePositive) {
       usefulResetCtr.resetZero()
     }.elsewhen(t2_needAllocate && !t2_canAllocate) {
-      usefulResetCtr.increase()
+      usefulResetCtr.selfIncrease()
     }
   }
 
@@ -572,9 +573,9 @@ class Tage(implicit p: Parameters) extends BasePredictor with HasTageParameters 
       assert(!(increase && decrease))
 
       when(increase) {
-        ctr.increase()
+        ctr.selfIncrease()
       }.elsewhen(decrease) {
-        ctr.decrease()
+        ctr.selfDecrease()
       }
     }
   }

src/main/scala/xiangshan/frontend/bpu/ubtb/MicroBtb.scala
@@ -181,7 +181,7 @@ class MicroBtb(implicit p: Parameters) extends BasePredictor with HasMicroBtbPar
   private def initEntryIfNotUseful(notUseful: Bool): Unit =
     when(notUseful) {
       t1_updatedEntry.tag := t1_tag
-      t1_updatedEntry.usefulCnt.resetPositive() // usefulCnt inits at strong positive, in/decrease by policy
+      t1_updatedEntry.usefulCnt.resetSaturatePositive() // usefulCnt inits at strong positive, in/decrease by policy
       // slot1
       t1_updatedEntry.slot1.position       := t1_position
       t1_updatedEntry.slot1.attribute      := t1_attribute
@@ -191,7 +191,7 @@ class MicroBtb(implicit p: Parameters) extends BasePredictor with HasMicroBtbPar
       // TODO: 2-taken train
       t1_updatedEntry.slot2.valid := false.B
     }.otherwise {
-      t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getDecrease
+      t1_updatedEntry.usefulCnt := t1_hitEntry.usefulCnt.getDecrease()
     }
 
   when(t1_fire) {
@@ -210,7 +210,7 @@ class MicroBtb(implicit p: Parameters) extends BasePredictor with HasMicroBtbPar
     }.otherwise {
       // everything matches, and actually taken
       // increase usefulCnt
-      t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getIncrease
+      t1_updatedEntry.usefulCnt := t1_hitEntry.usefulCnt.getIncrease()
     }
   }
 